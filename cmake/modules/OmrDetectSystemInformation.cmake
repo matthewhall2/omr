0f64b7f85cf77a6e6044cc636996ed1250438a7c
Gaurav Chaudhari <gaurav.chaudhari@ibm.com>
2024-04-08 11:10:42 -0700

    Added conditionals and adjustments for Open XL

--- cmake/modules/OmrDetectSystemInformation.cmake
+++ cmake/modules/OmrDetectSystemInformation.cmake
@@ -209,7 +209,7 @@ macro(omr_detect_system_information)
 				# depending on the CMake version. Without this check, it's ambiguous whether the
 				# compiler is XLC or XLClang.
 				set(CMAKE_C_COMPILER_IS_XLCLANG TRUE CACHE BOOL "XLClang is the C compiler")
-			elseif(CMAKE_C_COMPILER MATCHES ".*ibm-clang(/d/d)?$")
+			elseif(CMAKE_C_COMPILER MATCHES ".*ibm-clang.*")
 				set(CMAKE_C_COMPILER_IS_OPENXL ON CACHE BOOL "ibm-clang is the C compiler")
 				set(_OMR_TOOLCONFIG "openxl")
 			else()

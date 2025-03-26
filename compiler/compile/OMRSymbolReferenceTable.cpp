bc9292f3b92fb8854a87a4eaacf7b2ebea7766aa
Matthew Hall <matthew.hall3@outlook.com>
2025-03-26 11:13:19 -0400

    verbose
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- compiler/compile/OMRSymbolReferenceTable.cpp
+++ compiler/compile/OMRSymbolReferenceTable.cpp
@@ -1814,7 +1814,7 @@ OMR::SymbolReferenceTable::findOrCreatePendingPushTemporary(
    bool osrEnabled = owningMethodSymbol->comp()->getOption(TR_EnableOSR);
    uint32_t slotNum = slot + TR::Symbol::convertTypeToNumberOfSlots(type) - 1;
    TR_ASSERT_FATAL(!osrEnabled || slotNum < owningMethodSymbol->getNumuPPSlots(),
-      "cannot create a pending push temporary that exceeds the number of slots for this method:\nSlot: %d\nSlot Number: %d\nNumber of slots: %d\n", slot, slotNum, owningMethodSymbol->getNumuPPSlots());
+      "cannot create a pending push temporary that exceeds the number of slots for this method:\nSlot: %d\nSlot Number: %d\nData Type: %d\nNumber of slots: %d\n", slot, slotNum, type, owningMethodSymbol->getNumuPPSlots());
 #endif
    TR::SymbolReference *tempSymRef = findOrCreateAutoSymbol(owningMethodSymbol, -(slot + 1), type, true, false, false, false, size);
    tempSymRef->getSymbol()->setIsPendingPush();

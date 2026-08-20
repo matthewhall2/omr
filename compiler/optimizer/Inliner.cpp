135bccb32693e160d3cf5426a7926e25f09d2f50
Matthew Hall <matthew.hall3@outlook.com>
2026-08-20 10:24:59 -0400

    fatal witrh node

--- compiler/optimizer/Inliner.cpp
+++ compiler/optimizer/Inliner.cpp
@@ -2822,7 +2822,8 @@ void TR_HandleInjectedBasicBlock::findAndReplaceReferences(TR::TreeTop *callBBSt
 void TR_HandleInjectedBasicBlock::collectNodesWithMultipleReferences(TR::TreeTop *storeInsertionPoint,
     TR::TreeTop *start, TR::TreeTop *end)
 {
-    TR_ASSERT_FATAL(_multiplyReferencedNodes.getFirst() == NULL, "unexpected live nodes");
+   // TR_ASSERT_FATAL(_multiplyReferencedNodes.getFirst() == NULL, "unexpected live nodes");
+    TR_ASSERT_FATAL_WITH_NODE(_multiplyReferencedNodes.getFirst(), _multiplyReferencedNodes.getFirst() == NULL, "unexpected live nodes");
 
     // Ensure there will be no PassThroughs in _multiplyReferencedNodes.
     // The logic that generates the stores and loads is not prepared to

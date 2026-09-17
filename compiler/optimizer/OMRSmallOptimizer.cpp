f046c7e6b5cb3989ec2acea7db812c0d280543e3
Matthew Hall <matthew.hall3@outlook.com>
2026-09-17 13:20:29 -0400

    sanity revert preparenodeforremoal

--- compiler/optimizer/OMRSmallOptimizer.cpp
+++ compiler/optimizer/OMRSmallOptimizer.cpp
@@ -1547,7 +1547,7 @@ bool OMR::SmallOptimizer::prepareForNodeRemoval(TR::Node *node, bool deferInvali
     for (int32_t i = node->getNumChildren() - 1; i >= 0; i--) {
         TR::Node *child = node->getChild(i);
         if (child != NULL && child->getReferenceCount() == 1)
-            if (prepareForNodeRemoval(child, deferInvalidatingUseDefInfo))
+            if (prepareForNodeRemoval(child))
                 useDefInfoAreInvalid = true;
     }
     return useDefInfoAreInvalid;
